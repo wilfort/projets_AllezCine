67ebd9b32434fb30ebfeab6e085a754704beb3d8
wilfort <wilfort.stephane@hotmail.fr>
2018-06-28 20:58:59 +0200

    service worker

--- snippet.js
+++ snippet.js
@@ -11,6 +11,6 @@
   }*/
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./serv-work.js')
+             .register('./service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
