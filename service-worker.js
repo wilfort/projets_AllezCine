e06afa77469de97f67ef90a94b14050dd59e87d8
wilfort <wilfort.stephane@hotmail.fr>
2018-06-28 21:08:33 +0200

    service worker

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,15 @@
-var cacheAllo
-var dataCacheName = 'Syn-d-Loc';
-var urlsToCache = [
-  '.',
-  './index.html',
-  './assent/style.css',
-  './serv-work.js'
-];
+var cacheName = 'weatherPWA-step-6-1';
+var filesToCache = [];
 
 self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
-    e.waitUntil(
-      caches.open(cacheAllo).then(function(cache) {
-        console.log('[ServiceWorker] Caching app shell');
-        return cache.addAll(urlsToCache);
-      })
-    );
-  });
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
 
 // self.addEventListener('activate', function(e) {
 //     console.log('[ServiceWorker] Activate');
